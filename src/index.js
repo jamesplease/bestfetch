86353898937e6342af4289275b0130190aba3c9f
Conor Hastings <hastings.conorm@gmail.com>
2018-10-29 14:12:50 -0700

    return undefined if not in cache

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ export function isResponseCached(requestKey) {
 }
 
 export function getCachedResponse(requestKey) {
-  return responseCache[requestKey] || null;
+  return isResponseCached(requestKey) ? responseCache[requestKey] : undefined;
 }
 
 
