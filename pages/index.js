e7f5fa9c74ca34fb64d0a992bca354ffbc73116b
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-03-21 22:56:59 -0700

    Add landing page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import Link from '../components/link';
 
 export default function Index() {
   return (
-    <div className="page">
+    <div
+      className="page"
+      style={{
+        '--maxAppWidth': '1150px',
+      }}>
       <div className="hero">
         <h1 className="indexTitle">bestfetch</h1>
         <div className="badgeList">
@@ -31,6 +35,48 @@ export default function Index() {
             />
           </a>
         </div>
+        <div className="heroProps">
+          <div className="heroPropContainer">
+            <div className="heroEmoji">🏎</div>
+            <div className="heroProp">Fast</div>
+            <div className="heroDescription">
+              Speed up your app <i>considerably</i> by caching responses. Only
+              make network requests when you need to.
+            </div>
+          </div>
+          <div className="heroPropContainer">
+            <div className="heroEmoji">🐭</div>
+            <div className="heroProp">Small</div>
+            <div className="heroDescription">
+              bestfetch is tiny but mighty. With its reasonable file size (
+              <code>&lt;3kb</code> gzipped) you can feel responsible adding it
+              to an existing codebase.
+            </div>
+          </div>
+        </div>
+        <div className="heroProps">
+          <div className="heroPropContainer">
+            <div className="heroEmoji">⚙️</div>
+            <div className="heroProp">Flexible</div>
+            <div className="heroDescription">
+              bestfetch has great defaults that work for many apps. If something
+              doesn't work for your use case, change it: bestfetch's flexible
+              API keeps you in control.
+            </div>
+          </div>
+          <div className="heroPropContainer">
+            <div className="heroEmoji">⚡️</div>
+            <div className="heroProp">Efficient</div>
+            <div className="heroDescription">
+              bestfetch is great for your users, too: many people worldwide
+              browse the web on mobile phones with limited data plans. Use less
+              of their data by leveraging bestfetch.
+            </div>
+          </div>
+        </div>
+        <Link href="/getting-started">
+          <a className="getStartedLink">Get Started</a>
+        </Link>
       </div>
     </div>
   );
