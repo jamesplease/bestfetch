35712e9f9728b73502f071920e9aca3744f487dd
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-03-22 11:55:40 -0700

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -433,10 +433,11 @@ describe('cacheWritePolicy', () => {
   test('default does not cache 500 server errors', done => {
     bestfetch('/test/fails/internal-server-error', {
       requestKey: 'will-error',
+      responseType: 'text',
     }).then(res => {
       expect(res).toEqual(
         expect.objectContaining({
-          data: null,
+          data: 'Server error message',
           status: 500,
           statusText: 'Internal Server Error',
           ok: false,
@@ -447,10 +448,11 @@ describe('cacheWritePolicy', () => {
 
       bestfetch('/test/fails/internal-server-error', {
         requestKey: 'will-error',
+        responseType: 'text',
       }).then(resTwo => {
         expect(resTwo).toEqual(
           expect.objectContaining({
-            data: null,
+            data: 'Server error message',
             status: 500,
             statusText: 'Internal Server Error',
             ok: false,
