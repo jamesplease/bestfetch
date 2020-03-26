8236a8d207b0be0abef57221673f8731c84537e1
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-03-25 17:06:48 -0700

    Continue work on TS

--- src/cache-miss-error.ts
+++ src/cache-miss-error.ts
@@ -1,10 +1,36 @@
 // A CacheMissError represents a cache miss. The Promise returned by a bestfetch call
 // will reject to this if you specify `cachePolicy: "cache-only"` and there is nothing
 // found in the cache.
-export default class CacheMissError extends Error {
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, new.target.prototype);
-    this.name = CacheMissError.name;
-  }
+
+interface CacheMissErr {
+  new (msg: string): CacheMissErr;
+  value: typeof CacheMissError;
+  writable: boolean;
+  configurable: boolean;
+  name: string;
+  message: string;
 }
+
+interface FooConstructor {
+  new (message: string): CacheMissErr;
+  prototype: CacheMissErr;
+}
+
+const CacheMissError = (function CacheMissError(
+  this: CacheMissErr,
+  message: string
+) {
+  var err = Error.call(this, message);
+  err.name = this.name = 'CacheMissError';
+  this.message = err.message;
+} as Function) as FooConstructor;
+
+CacheMissError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: CacheMissError,
+    writable: true,
+    configurable: true,
+  },
+});
+
+export default CacheMissError;
